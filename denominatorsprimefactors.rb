4f6d99d5377ee5ff4e976f560c66a2adfd91f7d6
Norbert Reschke <reschke@hsbkob.de>
2018-02-15 23:10:37 +0100

    geändert:       denominatorsprimefactors.rb
    now the first column looks like it should

--- denominatorsprimefactors.rb
+++ denominatorsprimefactors.rb
@@ -4,20 +4,27 @@ require 'prime'
 
 n = [6,12,24,18,25,15] # ...for example
 
-r = n.reduce(1,:lcm) # we do this at first, because we need to know how many digits the result has.
+n.sort! # looks better, doesn't it?
+
+r = n.reduce(1,:lcm) # we do this at first, because we need to know how many digits the result has (look at w1c).
+
+w1c = r.to_s.length # width of first column (how many digits has the resulting number)
 
 n.each { |b|
-  print b,"|" # only eye-catching (in my opinion) not needed to get at way to our result
+  printf("%" + w1c.to_s + "s |",b) # only eye-catching (in my opinion) not needed to get at way to our result
   b.prime_division.each { |f,l| # prime_division gives us an array with the specific prime(s) and the index, how often this prime(s) appear; example 24 =>[[2,3],[3,1]]
     l.times do print " ",f end
   }
   print "\n"
 }
 
-print r,"|"
+(w1c+1).times do print "-" end
+print "+"
+
+print "\n"
+print r," |"
 
 a=1
-
 k = []
 n.each { |i|
   j = i.prime_division
