50603a483dfabaf955c95f376aed7600079a8131
Norbert Reschke <reschke@hsbkob.de>
2018-02-16 14:54:08 +0100

    geändert:       denominatorsprimefactors.rb
    now cell axis and cell (c) look right

--- denominatorsprimefactors.rb
+++ denominatorsprimefactors.rb
@@ -10,31 +10,40 @@ r = n.reduce(1,:lcm) # we do this at first, because we need to know how many dig
 
 w1c = r.to_s.length # width of first column (how many digits has the resulting number)
 
+a = 1
+k = []
+w2c = 0 # width of second column (this will be expanded)
+q = ""
+
+n.each { |i|
+  j = i.prime_division
+  k = k|j
+}
+
+p = k.group_by(&:first).map { |f,l| 
+  l.max
+}
+
 n.each { |b|
-  printf("%" + w1c.to_s + "s |",b) # only eye-catching (in my opinion) not needed to get at way to our result
+  printf("%" + w1c.to_s + "s ",b) # denominators (a)
+  print "|" # vertical lineparts        
   b.prime_division.each { |f,l| # prime_division gives us an array with the specific prime(s) and the index, how often this prime(s) appear; example 24 =>[[2,3],[3,1]]
     l.times do print " ",f end
   }
   print "\n"
 }
 
-(w1c+1).times do print "-" end
-print "+"
+(w1c+1).times do print "-" end # horizontal line parts on right side
+print "+" # intersection of horizontal and vertical lines 
+p.each { |f,l|
+  l.times do q.concat(" ",f.to_s) end # get primefactors for result
+  w2c = w2c + l*2    
+}
+ 
+w2c.times do print "-" end  # horizontal line parts on right side
 
 print "\n"
-print r," |"
-
-a=1
-k = []
-n.each { |i|
-  j = i.prime_division
-  k = k|j
-}
-
-k.group_by(&:first).map { |f,l| 
-  l.max
-}.each { |f,l|
-  l.times do print " ",f  end
-}
+print r    # result (d)
+print " |" # vertical line part
+print q    # primefactors for result (c)
 print "\n"
-
